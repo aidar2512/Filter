7c5ecbb634c407115201c9635c9037b05cf2ac6a
aidar2512 <gumasroid@gmail.com>
2024-11-27 11:05:16 +0600

    second

--- react-vite-filter/src/App.jsx
+++ react-vite-filter/src/App.jsx
@@ -4,6 +4,12 @@ function App() {
   const [data, setData] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div>
       <h1>Фильтрация данных</h1>
