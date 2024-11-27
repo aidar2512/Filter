d1744f74267d6c96039c3396e56f083a12cff0f0
aidar2512 <gumasroid@gmail.com>
2024-11-27 11:03:15 +0600

    first

---
+++ react-vite-filter/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <div>
+      <h1>Фильтрация данных</h1>
+    </div>
+  );
+}
+
+export default App;
